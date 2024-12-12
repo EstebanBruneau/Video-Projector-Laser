68dd2f990203ffb8ea2080977afc07ae75a94438
emma-degot <emma.degot@gmail.com>
2024-12-12 15:02:42 +0100

    interupt ok ?

--- test.cpp
+++ test.cpp
@@ -1,43 +1,14 @@
-// #include <vector>
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define LED_PIN 29
+#define LED_READ 17
+#define LED_WRITE 27
 
-int status = 0;
+int status=0;
 
-/*
-régler pin mode up et down
-simuler signal carré fréquence à def
-mesurer le temps de réponse
-utiliser un interrupt connecté à un pin
-il va déclencher une fonction qui active un pin (allume ou éteind (passe de 0 à 1 ou de 1 à 0
-stocker la valeur des pin dans des variables))
-definir l'interrupt (sur quel Pin)
-wiringpi ISR
-*/
-
-// std::vector<std::vector<int,int>> list_etat;
-
-void changement_etat(){
-    status = 1;
-    if(digitalRead(LED_PIN) == 0){
-        digitalWrite(LED_PIN, 1);
-    }else{
-        digitalWrite(LED_PIN, 0);
-    }
-}
-
-void myInterrupt0 (void) {
-    printf("Interrupt 0\n");
-    changement_etat();
-    return;
-}
-
-void myInterrupt1 (void) {
-    printf("Interrupt 1\n");
-    changement_etat();
-    return;
+void pinISR (void) {
+    status=!status;
+    digitalWrite(LED_WRITE, status);
 }
 
 int main(void){
@@ -46,22 +17,20 @@ int main(void){
         printf("Error setting up wiringPi\n");
         return 1;
     }
-    printf("WiringPi setup\n");
 
-    // wiringPiISR (0, INT_EDGE_RISING, &myInterrupt1) ;
-    // wiringPiISR (0, INT_EDGE_FALLING, &myInterrupt0) ;
+    pinMode(LED_READ, INPUT);
+    pinMode(LED_WRITE, OUTPUT);
 
-    pinMode(26, INPUT);
+    digitalWrite(LED_WRITE, status);
 
-    while(1){
-        // if (status == 1){
-        //     printf("Interrupt 0\n");
-        //     break;
-        // }
-        printf("%d\n", digitalRead(26));
-        delay(100);
+    if(wiringPiISR(LED_READ, INT_EDGE_BOTH, &pinISR)<0){
+        printf("Error setting up ISR\n");
+        return 1;
     }
 
-    
+    while(1){
+        delay(1000);
+    }
+
     return 0;
 }
