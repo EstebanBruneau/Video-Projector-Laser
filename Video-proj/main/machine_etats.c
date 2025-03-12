8bf25f132a2c98d4466d9128e10518a874bffd44
Este <esteban.bruneau77@gmail.com>
2025-03-12 14:45:24 +0100

    Refactor RGB pin definitions and data handling for improved clarity and maintainability

--- Video-proj/main/machine_etats.c
+++ Video-proj/main/machine_etats.c
@@ -14,10 +14,33 @@ void process_state(void);
 void IRAM_ATTR motor_rotation_isr(void* arg);
 void IRAM_ATTR mirror_change_isr(void* arg);
 
-#define MOTOR_PIN       4  // Pin for motor rotation detection
-#define MIRROR_PIN      5  // Pin for mirror position detection
-#define COLOR_START_PIN 22 // Starting pin for RGB control
-#define DATA_START_PIN  26 // Starting pin for 8-bit color data
+// Input pins
+#define MOTOR_PIN           4   // GPIO4  - Motor rotation detection
+#define MIRROR_PIN          5   // GPIO5  - Mirror position detection
+
+// RGB select pins
+#define RED_SELECT_PIN     22   // GPIO22 - Red color select
+#define GREEN_SELECT_PIN   23   // GPIO23 - Green color select
+#define BLUE_SELECT_PIN    25   // GPIO25 - Blue color select
+
+// 8-bit data bus pins
+#define DATA_PIN_0         26   // GPIO26 - Data bit 0 (LSB)
+#define DATA_PIN_1         27   // GPIO27 - Data bit 1
+#define DATA_PIN_2         28   // GPIO28 - Data bit 2
+#define DATA_PIN_3         29   // GPIO29 - Data bit 3
+#define DATA_PIN_4         30   // GPIO30 - Data bit 4
+#define DATA_PIN_5         31   // GPIO31 - Data bit 5
+#define DATA_PIN_6         32   // GPIO32 - Data bit 6
+#define DATA_PIN_7         33   // GPIO33 - Data bit 7 (MSB)
+
+// Update COLOR_START_PIN to use first RGB select pin
+#define COLOR_START_PIN    RED_SELECT_PIN
+
+// Create array of data pins for easier iteration
+static const uint8_t DATA_PINS[8] = {
+    DATA_PIN_0, DATA_PIN_1, DATA_PIN_2, DATA_PIN_3,
+    DATA_PIN_4, DATA_PIN_5, DATA_PIN_6, DATA_PIN_7
+};
 
 #define PIXEL_DELAY_US  10000 // 10ms delay between pixels
 #define PIXELS_PER_LINE 100
@@ -51,31 +74,30 @@ static void affiche_pixel(void) {
     uint8_t g = current_matrix[line_counter][pixel_counter][1];
     uint8_t b = current_matrix[line_counter][pixel_counter][2];
 
-    // Set RGB select pins
-    for(int i = 0; i < 3; i++) {
-        gpio_set_level(COLOR_START_PIN + i, 0);
-    }
+    // Set RGB select pins low
+    gpio_set_level(RED_SELECT_PIN, 0);
+    gpio_set_level(GREEN_SELECT_PIN, 0);
+    gpio_set_level(BLUE_SELECT_PIN, 0);
 
-    // Set 8-bit color data
-    // Red
+    // Set 8-bit color data for Red
     for(int i = 0; i < 8; i++) {
-        gpio_set_level(DATA_START_PIN + i, (r >> i) & 0x01);
+        gpio_set_level(DATA_PINS[i], (r >> i) & 0x01);
     }
-    gpio_set_level(COLOR_START_PIN, 1);
+    gpio_set_level(RED_SELECT_PIN, 1);
     esp_rom_delay_us(10);
 
-    // Green
+    // Set 8-bit color data for Green
     for(int i = 0; i < 8; i++) {
-        gpio_set_level(DATA_START_PIN + i, (g >> i) & 0x01);
+        gpio_set_level(DATA_PINS[i], (g >> i) & 0x01);
     }
-    gpio_set_level(COLOR_START_PIN + 1, 1);
+    gpio_set_level(GREEN_SELECT_PIN, 1);
     esp_rom_delay_us(10);
 
-    // Blue
+    // Set 8-bit color data for Blue
     for(int i = 0; i < 8; i++) {
-        gpio_set_level(DATA_START_PIN + i, (b >> i) & 0x01);
+        gpio_set_level(DATA_PINS[i], (b >> i) & 0x01);
     }
-    gpio_set_level(COLOR_START_PIN + 2, 1);
+    gpio_set_level(BLUE_SELECT_PIN, 1);
     esp_rom_delay_us(10);
 }
 
@@ -102,11 +124,11 @@ void init_machine_etats(void) {
 
     // Configure output pins
     io_conf.pin_bit_mask = 0;
-    for(int i = 0; i < 3; i++) {
-        io_conf.pin_bit_mask |= (1ULL << (COLOR_START_PIN + i));
-    }
+    io_conf.pin_bit_mask |= (1ULL << RED_SELECT_PIN);
+    io_conf.pin_bit_mask |= (1ULL << GREEN_SELECT_PIN);
+    io_conf.pin_bit_mask |= (1ULL << BLUE_SELECT_PIN);
     for(int i = 0; i < 8; i++) {
-        io_conf.pin_bit_mask |= (1ULL << (DATA_START_PIN + i));
+        io_conf.pin_bit_mask |= (1ULL << DATA_PINS[i]);
     }
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.intr_type = GPIO_INTR_DISABLE;
